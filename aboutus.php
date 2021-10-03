681853a8c4d907dc9f5cc4dac812f972ef06819a
Swati Balasaheb Ugale <91780216+SwatiBalasaheb@users.noreply.github.com>
2021-10-03 14:27:38 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,7 @@
+<h2 style="background:#99FF33;padding:5px" align="left">About us</h2>
+<center><h3> Designed & Developed By </h3>
+
+<p> Savitri Ugale </p>
+<p> Department of MTech(Industrial Mathematics with Computer Application)</p>
+</center>
+  
